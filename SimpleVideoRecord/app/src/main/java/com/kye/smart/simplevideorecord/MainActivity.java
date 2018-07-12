9594d18addbcccba1c503ded79be3c33a2b20257
linglianwei <3476586377@qq.com>
2018-07-12 20:55:29 +0800

    1,初始化项目

---
+++ SimpleVideoRecord/app/src/main/java/com/kye/smart/simplevideorecord/MainActivity.java
@@ -0,0 +1,13 @@
+package com.kye.smart.simplevideorecord;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
